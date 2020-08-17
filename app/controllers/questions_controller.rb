5a10fd67713c4cc2dc8e3e99b0eff9fd4239b1e5
leandro barroso <lrbarroso1984@gmail.com>
2020-08-17 14:04:01 -0300

    Challenge 05-01-02 - stupid coaching - complete solution

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,9 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    @question = params[:question]
+    @answer = helpers.coach_answer_enhanced(@question)
+  end
+end
