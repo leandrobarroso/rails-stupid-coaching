66597e5a7c22c0d2328d526dd0dab44d4f5a9f39
leandro barroso <lrbarroso1984@gmail.com>
2020-08-21 21:14:48 -0300

    Challenge 05-01-02 - stupid coaching - style adjusted and code refactored

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,6 +3,30 @@ class QuestionsController < ApplicationController
 
   def answer
     @question = params[:question]
-    @answer = helpers.coach_answer_enhanced(@question)
+    @answer = coach_answer_enhanced(@question)
+  end
+
+  private
+
+  def coach_answer(your_message)
+    she_wanna_hear = 'I am going to work'
+    if your_message == she_wanna_hear
+      'Great!'
+    elsif your_message[-1] == '?'
+      'Silly question, get dressed and go to work!'
+    else
+      "I don't care, get dressed and go to work!"
+    end
+  end
+
+  def coach_answer_enhanced(your_message)
+    she_wanna_hear = 'I am going to work'
+    if your_message == she_wanna_hear.upcase
+      'Great!'
+    elsif your_message == your_message.upcase
+      "I can feel your motivation! #{coach_answer(your_message)}"
+    else
+      coach_answer(your_message)
+    end
   end
 end
